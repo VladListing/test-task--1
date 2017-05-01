843dd0591b1fff8aa72152a3e89a0394ca97f1e2
VladListing <chvch@mail.ru>
2017-05-01 20:33:51 +0700

    1

--- test-task--1/test-task--1/Program.cs
+++ test-task--1/test-task--1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region пространства имен
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,15 @@ using System.Runtime.InteropServices;
 using System.Collections.ObjectModel;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+#endregion
 
-namespace test_task__1  // создание бинарных файлов содержащих структуры описанные в тестовом задании
-
+namespace test_task__1  // создание бинарных файлов содержащих структуру "trade" описанную в тестовом задании
 {
-    
-    //====================================================== обьявляем структуры ================ start  ==================================|
-      #region описание структура trade
-    
-    [StructLayout(LayoutKind.Sequential, Pack = 1)] // размещение в неуправляемый код
-    public struct TradeRecord //обьявление структуры "TradeRecord"
+    //описание структуры 'TradeRecord' 
+    #region 'TradeRecord'
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]// размещение в неуправляемый код
+    public struct TradeRecord
     {
 
         public int id;
@@ -34,60 +34,45 @@ namespace test_task__1  // создание бинарных файлов сод
             account = b;
             volume = c;
             comment = d;
-
         }
+
     }
     #endregion
-    //====================================================== обьявляем структуры ================ end  =================================|
 
 
     class Program
     {
-
         static void Main(string[] args) // точка входа
         {
-            int schetchik = 0;//переменная счетчик количества проходов цикла
-            int quantityTradeRecodLine = 110;//переменная количества элементов масива  содержашего структуры типа TradeRecod----х
+            Console.WriteLine("");
+            Console.WriteLine("Введите количество строк для создаваемого бинарного файла  'D:\\Trade.dat' : ");
+            Console.WriteLine("");
+            string r = Console.ReadLine();
+            int quantityTradeRecodLine = Convert.ToInt32(r);//количество элементов массива  содержашего структуры типа TradeRecod----х
+            string path = @"D:\\Trade.dat";  //путь и имя будующего бинарного файла содержащего  структуры
+            int schetchik = 0;//счетчик
+            RandomString rnd = new RandomString();
 
-            //======================= создаем экземпляры (обьекты) структур и инициализируем их поля ============= start ==============================|
-            
-            //------------------------------------------------trade-------------------|
-            #region trade
-            //TradeRecord[] trade = new TradeRecord[7]; // создание экземпяра структуры "TradeRecord" на 7-мь строк
-            //trade[0] = new TradeRecord(01, 771, 6401, "profit 1");// инециализируем поля , присваиваем им значения через конструктор
-            //trade[1] = new TradeRecord(02, 772, 6402, "profit 2");
-            //trade[2] = new TradeRecord(03, 773, 6403, "profit 3");
-            //trade[3] = new TradeRecord(04, 774, 6404, "profit 4");
-            //trade[4] = new TradeRecord(05, 775, 6405, "profit 5");
-            //trade[5] = new TradeRecord(06, 776, 6406, "profit 6");
-            //trade[6] = new TradeRecord(07, 777, 6407, "profit 7");
+            //создание экземпляра структуры и инициализфция полей (присвоение значений)
+            #region 
 
             TradeRecord[] trade = new TradeRecord[quantityTradeRecodLine]; // создание экземпяра структуры "TradeRecord" на X строк
-            //TradeRecord[] newtrade = new TradeRecord[quantityTradeRecodLine]; // создание экземпяра структуры "TradeRecord" на X строк
-
 
             for (int i = 0; i < quantityTradeRecodLine; i++)
             {
-                trade[i] = new TradeRecord(0 + i, 771 + i, 640 + i, "profit " + i);// инециализируем поля , присваиваем им значения через конструктор
+                trade[i] = new TradeRecord(0 + i, 777, 640 + i, rnd.ArrayRand());
             }
             #endregion
-            //-------------------------------------------------------------------|
-
-            //============= создаем экземпляры (обьекты) структур и инициализируем их поля ======================= end ==========================|
 
 
-
-            //============================================= записываем значения из структур в бинарный файл Х*.dat ============== start =======================================|
-            string path = @"D:\\_LISTING_\B-files\StructTrade_Line_110.dat";  //путь и имя будующего бинарного файла содержащего  структуры
-
-
-            try//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''начало ''''|
+            //секция критичная в части исключений
+            try
             {
-                //================== создаем объект BinaryWriter (запись  в бинарный файл)=================================start=========================|
+                //создание экземпляра BinaryWriter (запись  в бинарный файл)
                 #region BinaryWriter
-                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))// открывае поток для записи структур в файл
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))// открывает поток для записи структур в файл
                 {
-                    //--записываем через цикл в файл значение каждого поля строк структуры "TradeRecord"-----------start------------------|
+
                     #region
                     foreach (TradeRecord t in trade)
                     {
@@ -98,23 +83,19 @@ namespace test_task__1  // создание бинарных файлов сод
 
                         schetchik = schetchik + 1;
                     }
-
-                    Console.WriteLine("Счетчик записанных в файл строк строк структуры trade: {0}  ", schetchik);
+                    Console.WriteLine("");
+                    Console.WriteLine("в бинарный файл 'D:\\Trade.dat' записано   {0}  строк(и) структуры 'trade'", schetchik);
 
                     schetchik = 0;
 
-                    
                     #endregion
-                    //-записываем через цикл в файл значение каждого поля строк структуры "TradeRecord"-----------------------end------|
+
                 }
-                # endregion
-                //================== создаем объект BinaryWriter (запись  в бинарный файл)================================= end =====================|
-                     
-                
-            }//'''''''''  try '''''''' оператор выполнение которого может привести к ошибке ''''''''''''конец ''''|
+                #endregion
 
+            }
 
-            //-----вывод сообщения о возникшем исключении--------
+            //вывод сообщения о возновении исключения
             #region исключения
             catch (Exception m)
             {
@@ -122,9 +103,9 @@ namespace test_task__1  // создание бинарных файлов сод
             }
             Console.ReadLine();
             #endregion
-            //---------------------------------------------------
 
-            //====================================== записываем значения из структур в бинарный файл .*dat ============================ end =============================|
+
+
 
         }
     }
