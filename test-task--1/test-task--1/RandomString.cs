2cea57c159b2d453f21335ee42f45ef1f1faae81
VladListing <chvch@mail.ru>
2017-05-01 20:39:30 +0700

    01-05-2017  генерирует заданное через консоль количество строк в винарный файл

--- test-task--1/test-task--1/RandomString.cs
+++ test-task--1/test-task--1/RandomString.cs
@@ -8,5 +8,40 @@ namespace test_task__1
 {
     class RandomString
     {
+        // в случайном порядке заполняем столбец коментариев
+        #region
+
+
+        Random r = new Random();
+
+        string sumstroka = "";
+        int s = 0;
+        int z = 0;
+        int t = 0;
+        public string ArrayRand()
+        {
+            s = r.Next(0, 6);
+            t = r.Next(-1000, 0);
+            z = r.Next(0, 10000);
+
+            if (s == 0)
+            { sumstroka = "   trade:Sell   result:Profit     " + "   +" + z + " $"; }
+            else if (s == 1)
+            { sumstroka = "   trade:Sell   result:Loss           " + t + " $"; }
+            else if (s == 2)
+            { sumstroka = "   trade:Bay    result:Profit     " + "  +" + z + " $"; }
+            else if (s == 3)
+            { sumstroka = "   trade:Bay    result:Loss          " + t + " $"; }
+            else if (s == 4)
+            { sumstroka = "   trade:Bay    result:StopLoss  " + t + " $"; }
+            else if (s == 5)
+            { sumstroka = "   trade:Sell   result:Stoploss  " + t + " $"; }
+
+            else
+            { sumstroka = " - "; }
+
+            return sumstroka;
+        }
+        #endregion
     }
 }
