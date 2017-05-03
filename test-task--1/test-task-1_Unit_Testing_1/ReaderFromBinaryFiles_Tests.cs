fff16e9b7dd2cef8b08f0af033ff501f661a6051
VladListing <chvch@mail.ru>
2017-05-03 22:20:50 +0700

    03-05-2017  добаление первого юнит теста

---
+++ test-task--1/test-task-1_Unit_Testing_1/ReaderFromBinaryFiles_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_task_1_Unit_Testing_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //---------------Unit test------------------------------------начало--------------------
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+
+
+
+
+
+        }
+        //---------------Unit test------------------------------------конец--------------------
+    }
+}
